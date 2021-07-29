1f1062389ecda916be0b98c105c68fa8c5c8c87a
avanti <avantigupta3@gmail.com>
2021-07-29 12:58:47 +0530

    palindrome checker

---
+++ script.js
@@ -0,0 +1,15 @@
+function palindrome() {
+  var num = document.querySelector(".inputbox").value.toUpperCase();
+
+  var alpha = num.split("").reverse().join("");
+
+  if (num.length <= 2) {
+    document.querySelector(".list").innerHTML = "TYPE MORE VALUES";
+  } else if (num.length >= 3 && num === alpha) {
+    document.querySelector(".list").innerHTML = `"${num}" is a Palindrome!`;
+  } else {
+    document.querySelector(
+      ".list"
+    ).innerHTML = `<b>"${num}"</b> is not a palindrome!`;
+  }
+}
